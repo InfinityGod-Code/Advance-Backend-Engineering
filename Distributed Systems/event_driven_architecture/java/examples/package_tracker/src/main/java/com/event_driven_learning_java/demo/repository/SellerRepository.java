0a5dfb37cc208b52de14baf60c4d2606bb44f1f6
Shubham Srivastava <codetosuccessyt@gmail.com>
2026-06-23 15:30:30 +0530

    package-pro mono repo

--- Distributed Systems/event_driven_architecture/java/examples/package_tracker/src/main/java/com/event_driven_learning_java/demo/repository/SellerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.event_driven_learning_java.demo.repository;
-
-import com.event_driven_learning_java.demo.entity.Seller;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerRepository extends JpaRepository<Seller, Long> {
-}
--- Distributed Systems/event_driven_architecture/java/examples/package_tracker/src/main/java/com/event_driven_learning_java/demo/repository/ShipmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.event_driven_learning_java.demo.repository;
-
-import com.event_driven_learning_java.demo.entity.Shipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
-}
